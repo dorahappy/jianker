940f869e6276b2fb829f75bb89b3af899db59b7a
zhangxiaowei <15122058799@163.com>
2017-12-26 23:18:45 +0800

    新建了banner组件和footer组件

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,18 @@
   <div id="app">
     <app-header></app-header>
     <app-banner></app-banner>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
 import AppHeader from './components/index/AppHeader'
 import AppBanner from './components/index/AppBanner'
+import AppFooter from './components/index/AppFooter'
 export default {
   name: 'app',
   components:{
-    AppHeader,AppBanner
+    AppHeader,AppBanner,AppFooter
   }
 }
 </script>
---
+++ src/components/index/AppFooter.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="app-footer">
+        <dl>
+            <dt class="yo-ico">&#xe604;</dt>
+            <dd>首页</dd>
+        </dl>
+        <dl>
+            <dt class="yo-ico">&#xe606;</dt>
+            <dd>全部兼职</dd>
+        </dl>
+        <dl>
+            <dt class="yo-ico">&#xe621;</dt>
+            <dd>智学堂</dd>
+        </dl>
+        <dl>
+            <dt class="yo-ico">&#xe502;</dt>
+            <dd>消息</dd>
+        </dl>
+        <dl>
+            <dt class="yo-ico">&#xe605;</dt>
+            <dd>我的</dd>
+        </dl>
+    </div>
+</template>
+<script>
+export default {
+    name:"app-footer"
+}
+</script>
+<style lang="sass" scoped>
+
+</style>
