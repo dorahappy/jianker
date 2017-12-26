8b6eea7685e5509ec01c6939e29ac6325fb30dc4
zhangxiaowei <15122058799@163.com>
2017-12-26 21:42:35 +0800

    配置了yo3

---
+++ src/components/index/AppBanner.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="app-banner">
+        <ul>
+            <li>
+                <!-- <img src="https://img1.360buyimg.com/da/jfs/t12022/221/2537221377/323613/4429db73/5a421fbbNc96ef514.jpg" alt=""> -->
+                111111
+            </li>
+            <li>222</li>
+            <li>222</li>
+            <li>333</li>
+        </ul>
+        <div>
+            
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'app-banner',
+
+}
+</script>
+<style lang='scss' scoped>
+    
+    
+</style>
