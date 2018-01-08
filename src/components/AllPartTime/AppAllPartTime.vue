9fa7dc29c9c2ecf96c719b95f93ed01250615c91
zhangxiaowei <15122058799@163.com>
2018-01-08 21:21:30 +0800

    添加了vuex

--- src/components/AllPartTime/AppAllPartTime.vue
+++ src/components/AllPartTime/AppAllPartTime.vue
@@ -1,13 +1,22 @@
 <template>
     <div class="app-allparttime">
         allparttime全部兼职
+        
     </div>
 </template>
 
 <script>
 export default {
     name: 'app-allparttime',
-  
+    mounted(){
+
+    },
+    computed:{
+        
+    },
+    methods:{
+        
+    }
 }
 </script>
 
--- src/components/index/AppHome.vue
+++ src/components/index/AppHome.vue
@@ -14,7 +14,7 @@ export default {
     name: 'app-home',
     components:{
         AppHeader,AppBanner,AppListType
-    }
+    },
 }
 </script>
 
