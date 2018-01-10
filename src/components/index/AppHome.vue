d6a64db33e9e7298f7c336699920bcadbfa6543a
paney <zhongluping129@163.com>
2018-01-10 18:22:21 +0800

    启动页以及首页底部完成

--- src/components/index/AppHome.vue
+++ src/components/index/AppHome.vue
@@ -3,7 +3,11 @@
         <app-header></app-header>
         <app-banner></app-banner>
         <app-listType></app-listType>
-        <app-Content></app-Content>
+        <app-content></app-content>
+        <transition leave-active-class="animated fadeOut">
+        	<div class="app-start" v-if='isShow'></div>
+        </transition>
+        
     </div>
 </template>
 
@@ -16,10 +20,31 @@ export default {
     name: 'app-home',
     components:{
         AppHeader,AppBanner,AppListType,AppContent
+    },
+    data(){
+    	return{
+    		isShow:true
+    	}
+    },
+    mounted(){
+    	let that = this;
+    	setTimeout(function(){
+    		that.isShow = false;
+    	},2000)
     }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+	.app-start{
+	    width:100%;
+	    height:100%;
+	    z-index:1000;
+	    position:fixed;
+	    top:0;
+	    left:0;
+	    background:url(/static/img/index/start.png) no-repeat;
+	    background-size:cover;
+	}
 
 </style>
