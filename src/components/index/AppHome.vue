3f9877a6845192a23bd15d2bad195c84c6e035e8
paney <zhongluping129@163.com>
2018-01-11 21:39:30 +0800

    个人中心静态已完成

--- src/components/index/AppHome.vue
+++ src/components/index/AppHome.vue
@@ -3,11 +3,10 @@
         <app-header></app-header>
         <app-banner></app-banner>
         <app-listType></app-listType>
-        <app-content></app-content>
+        <app-Content></app-Content>
         <transition leave-active-class="animated fadeOut">
-        	<div class="app-start" v-if='isShow'></div>
+        	<div class="appStart" v-if="isShow"></div>
         </transition>
-        
     </div>
 </template>
 
@@ -18,33 +17,38 @@ import AppListType from './AppListType'
 import AppContent from './AppContent'
 export default {
     name: 'app-home',
+    data(){
+    	return{
+    		isShow:false
+    	}
+    	
+    },
     components:{
         AppHeader,AppBanner,AppListType,AppContent
     },
-    data(){
-    	return{
-    		isShow:true
-    	}
-    },
-    mounted(){
-    	let that = this;
-    	setTimeout(function(){
-    		that.isShow = false;
-    	},2000)
-    }
+    beforeRouteEnter(to, from, next) {
+	  next((vm)=>{
+	  	console.log(vm)
+	  	if(from.name == null){
+	  		vm.isShow = true
+	  		setTimeout(function(){
+	  			vm.isShow = false
+	  		},2000)
+	  	}
+	  })
+	}	
 }
 </script>
 
 <style lang="scss" scoped>
-	.app-start{
-	    width:100%;
-	    height:100%;
-	    z-index:1000;
-	    position:fixed;
-	    top:0;
-	    left:0;
-	    background:url(/static/img/index/start.png) no-repeat;
-	    background-size:cover;
+	.appStart{
+		z-index:999;
+		width:100%;
+		height:100%;
+		position:fixed;
+		top:0;
+		left:0;
+		background:url(/static/img/index/start.png);
+		background-size:cover;
 	}
-
 </style>
