23a39b5507a49bcbb347fb94287e65bdd00639af
zhangxiaowei <15122058799@163.com>
2018-01-11 23:12:58 +0800

    更改头部布局信息，增加头部背景色层

--- src/components/index/AppHeader.vue
+++ src/components/index/AppHeader.vue
@@ -1,20 +1,23 @@
 <template>
   <header class="app-head">
-  	<div class="city">
-  		<span>北京</span>
-  		<i class="yo-ico">&#xf031</i>
-  	</div>
-  	
-  	<div class="search">
-  		<div>
-  			<i class="yo-ico">&#xf067</i>
-  			<input type="text" placeholder="大家都在搜：兼职" />
-  		</div>
-  	</div>
-  	
-  	<div class="daytime">
-  		签到
-  	</div>
+		<div class="head-shadow"></div>
+		<div class="head-box">
+			<div class="city">
+				<span>北京</span>
+				<i class="yo-ico">&#xf031</i>
+			</div>
+			
+			<div class="search">
+				<div>
+					<i class="yo-ico">&#xf067</i>
+					<input type="text" placeholder="大家都在搜：兼职" />
+				</div>
+			</div>
+			
+			<div class="daytime">
+				签到
+			</div>
+		</div>
   </header>
 </template>
 <script>
