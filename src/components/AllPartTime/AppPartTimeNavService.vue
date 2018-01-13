27eded6ceb2d8ccdeadd19f362f8a4969ec0ece1
paney <zhongluping129@163.com>
2018-01-13 11:27:02 +0800

    全部兼职静态页面布局已完成

---
+++ src/components/AllPartTime/AppPartTimeNavService.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="app-parttime-nav-service">
+	    <div v-on:click="showClick">
+	        <p v-text="partworks[selected].text"></p><i></i>
+	        <ul style="display: none;" v-show="show" >
+	            <li @click="select(partworkIndex)" v-for="(partwork, partworkIndex) in partworks" >{{partwork.text}} </li>
+	        </ul>
+	    </div> 
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app-parttime-nav-service',
+    data:function(){
+    	 return {
+        show: false,
+        partworks:[
+        	{text:'客服'},
+        	{text:'短期兼职'}
+        ],
+        
+        selected: 0, // 代表選中的索引
+      }
+    },
+    methods: {
+      showClick() {
+        this.show = !this.show
+      },
+      select(index) {
+        this.selected = index
+      }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
---
+++ src/components/AllPartTime/AppPartTimeNavWork.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="app-parttime-nav-work">
+	    <div v-on:click="showClick">
+	        <p v-text="partworks[selected].text"></p><i></i>
+	        <ul style="display: none;" v-show="show" >
+	            <li @click="select(partworkIndex)" v-for="(partwork, partworkIndex) in partworks" >{{partwork.text}} </li>
+	        </ul>
+	    </div> 
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app-parttime-nav-work',
+    data:function(){
+    	 return {
+        show: false,
+        partworks:[
+        	{text:'长期兼职'},
+        	{text:'短期兼职'}
+        ],
+        
+        selected: 0, // 代表選中的索引
+      }
+    },
+    methods: {
+      showClick() {
+        this.show = !this.show
+      },
+      select(index) {
+        this.selected = index
+      }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
